38ba582df50efdda276056ccf48a982e7f1158b2
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2017-01-28 19:54:45 -0600

    Added delete

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
@@ -7,4 +7,21 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to :articles
+  end
+
 end
